e5fc844ca38d2131517a90db9f36c8f62d23b15f
calebpendev <calebpendev@gmail.com>
2021-08-19 16:06:40 -0700

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import List from './components/List';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state = {
+        list: [
+            {
+                id: 1,
+                amount: 0,
+                deleted: false
+            },
+            {
+                id: 2,
+                amount: 0,
+                deleted: false
+            },
+            {
+                id: 3,
+                amount: 0,
+                deleted: false
+            },
+        ]
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello World</h1>
+                <List list={this.state.list} />
+            </div>
+        );
+    }
 }
-
 export default App;
---
+++ src/components/List.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ListItem from './ListItem';
+import PropTypes from 'prop-types';
+
+class List extends Component {
+    render() {
+        return this.props.list.map((list) => (
+            <ListItem key={ list.id } list={list} />
+            ));
+    }
+}
+
+List.propTypes = {
+    counters: PropTypes.array.isRequired
+}
+export default List;
